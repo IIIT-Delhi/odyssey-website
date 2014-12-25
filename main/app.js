d8f3e572f556eeb0511de7345c775019689d9348
root <root@esya.iiitd.edu.in>
2014-12-26 00:49:27 +0530

    deploy

--- main/app.js
+++ main/app.js
@@ -1,46 +1,44 @@
 var express = require('express'),
     app = express();
-    //nodemailer = require('nodemailer');
 
-app.use(express.static('.'));
+app.use(express.static('./WEB/WebFiles/'));
 
 app.get('/', function(req, res){
-	res.sendFile('page/index.html');});
+	res.sendFile('/root/odyssey/main/WEB/Fishy.html');});
 
 app.get('/lifestyle', function(req, res){
-	res.sendFile('page/lifestyle.html');});
+	res.sendFile('/root/odyssey/main/page/lifestyle.html');});
 
 app.get('/gaming', function(req, res){
-	res.sendFile('page/gaming.html');});
+	res.sendFile('/root/odyssey/main/page/gaming.html');});
 
 app.get('/digitalarts', function(req, res){
-	res.sendFile('page/digitalarts.html');});
+	res.sendFile('/root/odyssey/main/page/digitalarts.html');});
 
 app.get('/finearts', function(req, res){
-	res.sendFile('page/finearts.html');});
+	res.sendFile('/root/odyssey/main/page/finearts.html');});
 
 
 app.get('/music', function(req, res){
-	res.sendFile('page/music.html');});
+	res.sendFile('/root/odyssey/main/page/music.html');});
 
 
 app.get('/dance', function(req, res){
-	res.sendFile('page/dance.html');});
+	res.sendFile('/root/odyssey/main/page/dance.html');});
 
 app.get('/chess', function(req, res){
-	res.sendFile('page/chess.html');});
+	res.sendFile('/root/odyssey/main/page/chess.html');});
 
 app.get('/treasurehunt', function(req, res){
-	res.sendFile('page/treasurehunt.html');});
+	res.sendFile('/root/odyssey/main/page/treasurehunt.html');});
 
 app.get('/literary', function(req, res){
-	res.sendFile('page/literary.html');});
+	res.sendFile('/root/odyssey/main/page/literary.html');});
 
 app.get('/quizzing', function(req, res){
-	res.sendFile('page/quizzing.html');});
+	res.sendFile('/root/odyssey/main/page/quizzing.html');});
 
 app.get('/theatre', function(req, res){
-	res.sendFile('page/theatre.html');});
-
+	res.sendFile('/root/odyssey/main/page/theatre.html');});
 
 app.listen(80);
