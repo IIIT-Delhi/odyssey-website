6df32fd631bc19b4005396ca91e673731fb70813
atul10595 <atul13025@iiitd.ac.in>
2014-12-28 21:03:39 +0530

    app.listen(80)

--- main/app.js
+++ main/app.js
@@ -7,7 +7,7 @@ var path = require('path');
 //app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -30,7 +30,5 @@ app.get('/music', routes.music);
 
 app.get('/team', routes.team);
 
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(80);
 
